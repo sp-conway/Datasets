b8b86ce05690530314aa5d5570e309def7969069
Sean Conway <58159926+sp-conway@users.noreply.github.com>
2021-08-05 14:26:10 -0400

    Update eggs_data_prep.R

--- R/eggs_data_prep.R
+++ R/eggs_data_prep.R
@@ -17,10 +17,10 @@ year_var <- map2(year, n, rep) %>%
 eggs_1 <- eggs %>% 
   rename(
     month=`...1`,
-         extra_large_dozen=`Extra Large \nDozen`,
-         extra_large_half_dozen=`Extra Large 1/2 Doz.\n1/2 Dozen`,
-         large_dozen=`Large \nDozen`,
-         large_half_dozen = `Large \n1/2 Doz.`
+    extra_large_dozen=`Extra Large \nDozen`,
+    extra_large_half_dozen=`Extra Large 1/2 Doz.\n1/2 Dozen`,
+    large_dozen=`Large \nDozen`,
+    large_half_dozen = `Large \n1/2 Doz.`
   ) %>%
   mutate(
     year=year_var,
